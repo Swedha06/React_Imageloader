8b902341b2324b8eca6e21720773a9216f6475c9
Swedha06 <swedhamal12@gmail.com>
2022-12-12 08:08:41 -0800

    image loader project added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import {useState} from "react";
+import "./App.css";
+function App()
+{
+    const[msg,finalmsg]=useState([])
+    const[arr,finalarr]=useState([
+      {
+        URL1:"",
+        URL2:"",
+        URL3:""
+      }
+    ])
+    function handleChange(e)
+    {
+      finalarr({...arr,[e.target.name]:e.target.value})
+    }
+    let URL1=arr.URL1;
+    let URL2=arr.URL2;
+    let URL3=arr.URL3;
+    function handleSubmit(e) {
+      e.preventDefault();
+      finalmsg([...msg,{URL1,URL2,URL3}])
+      finalarr({URL1:"",URL2:"",URL3:""})
+    }
+    return(
+      <div className="App">
+        
+          <h1>Image Loader</h1>
+          <div className="fix-table">
+          <p>Image URL 1</p>
+          <input type="url" name='URL1' value={arr.URL1} onChange={handleChange} placeholder="Enter the image URL 1"></input><br></br>
+  
+          <p>Image URL 2</p>
+          <input type="url" name='URL2' value={arr.URL2} onChange={handleChange} placeholder="Enter the image URL 2"></input><br></br>
+  
+          <p>Image URL 3</p>
+          <input type="url" name='URL3' value={arr.URL3} onChange={handleChange} placeholder="Enter the image URL 3"></input><br></br>
+  
+          <button onClick={handleSubmit}>create</button>
+        </div>
+        <h1>Image List</h1>
+        <div className="fit-image">
+          <div>
+            {
+              msg.map((figure,id)=>{
+                  return(
+                    
+                      <div key={id}>
+                      <div className="set"> 
+                      <img src={figure.URL1} alt="new"/>
+                      </div>
+  
+                      <div className="set">
+                      <img src={figure.URL2} alt="new" />
+                      </div>
+  
+                      <div className="set">
+                      <img src={figure.URL3} alt="new" />
+                      </div>
+                      </div>    
+                  )
+                }
+              )
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
 
 export default App;
